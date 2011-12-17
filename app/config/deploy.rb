e5201ceb6c334ad0b58f0e1e83b28aa0b9baabd1
John <vertigojt@gmail.com>
2011-12-18 01:44:43 +0400

    Second?!

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -3,10 +3,15 @@ set   :deploy_to,     "/home/vertigo2/projects/sym_test"
 set   :domain,        "sym_test"
 
 set   :scm,           :git
-set   :repository,    "git://github.com/jtvertigo/Capify_test.git"
+set   :repository,    "file:///home/vertigo2/projects/symfony_capifony"
 set   :deploy_via,    :copy
 
-role  :server,        domain
+role  :web,           domain
+role  :app,           domain
+role  :db,            domain, :primary => true
+
+set   :deploy_via,    :rsync_with_remote_cache
+set   :php_bin,       "/opt/lampp/bin/php"
 
 set   :use_sudo,      false
 set   :keep_releases, 3
