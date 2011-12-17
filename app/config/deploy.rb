b98fff4b36fec82d3ad5588f090e0962aa19984d
John <vertigojt@gmail.com>
2011-12-18 03:08:04 +0400

    Fifth?

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -6,10 +6,25 @@ set   :scm,           :git
 set   :repository,    "file:///home/vertigo2/projects/symfony_capifony"
 set   :deploy_via,    :copy
 
-role  :server,           domain
+role  :server,        domain
 
 set   :deploy_via,    :rsync_with_remote_cache
+
+# PHP bin path on remote server
 set   :php_bin,       "/opt/lampp/bin/php"
 
 set   :use_sudo,      false
 set   :keep_releases, 3
+
+# ./bin/vendors
+set   :update_vendors, false
+
+# SSH-user
+# set :user, "vertigo2"
+
+# SSH-password
+# set :password, "vert123"
+
+# set environment ?!
+# set :symfony_env_prod, "prod"
+# set :symfony_env_dev,  "dev"
