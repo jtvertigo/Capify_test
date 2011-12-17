7607e47baf8760c2cae5053497fb668d0353f774
John <vertigojt@gmail.com>
2011-12-17 23:01:15 +0400

    Forth commit

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,17 +1,12 @@
-set :application, "set your application name here"
-set :domain,      "#{application}.com"
-set :deploy_to,   "/var/www/#{domain}"
-set :app_path,    "app"
+set   :application,   "sym_test"
+set   :deploy_to,     "/home/vertigo2/projects/sym_test"
+set   :domain,        "sym_test"
 
-set :repository,  "#{domain}:/var/repos/#{application}.git"
-set :scm,         :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, `subversion` or `none`
+set   :scm,           :git
+set   :repository,    "git://github.com/jtvertigo/Capify_test.git"
+set   :deploy_via,    :copy
 
-set :model_manager, "doctrine"
-# Or: `propel`
+role  :server,        domain
 
-role :web,        domain                         # Your HTTP server, Apache/etc
-role :app,        domain                         # This may be the same as your `Web` server
-role :db,         domain, :primary => true       # This is where Rails migrations will run
-
-set  :keep_releases,  3
+set   :use_sudo,      false
+set   :keep_releases, 3
